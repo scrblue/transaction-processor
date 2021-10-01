7f1fcba1b1485257e051c76543207d1577b82587
Daniel Lyne <DLyne@pm.me>
2021-09-30 20:14:31 -0400

    Add basic types and traits

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+mod reader;
+
+enum TransactionType {
+	Deposit,
+	Withdrawal,
+	Dispute,
+	Resolve,
+	Chargeback,
+}
+
+/// A single transaction to be processed by the application
+struct Transaction {
+    /// Types including deposits, withdrawals, disputes, resolutions of disputes, and chargebacks
+	ty: TransactionType,
+
+	/// A unique client ID for which all transactions are tied to
+	client: u16,
+
+	/// A unique transaction ID given to deposits or withdrawals. Disputes, resolutions, and
+	/// chargebacks reference transaction IDs of deposits
+	tx: u32,
+
+	/// The amount of the deposit or withdrawal. This field will be None for any other TransactionType
+	amount: Option<i64>,
+}
+
+
+/// A single client's data to be output by the application
+struct Client {
+    /// The total funds available for withdrawal or other use.
+	available: i64,
+
+	/// The total funds held for dispute
+	held: i64,
+
+	/// The total funds of the account disputed or not. Equal to available + held.
+	total: i64,
+
+	/// Whether the account has been locked after a chargeback
+	locked: bool,
+}
+
 fn main() {
     
 }
